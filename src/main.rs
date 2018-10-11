8371dbac24c813081d914c50a1882043a22d6a3c
David Nguyen <hpcsc@users.noreply.github.com>
2018-10-11 23:52:57 +0800

    add profile pattern arg to set

--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,47 @@ fn main() {
     let yaml = load_yaml!("cli.yaml");
     let matches = App::from_yaml(yaml).get_matches();
 
-    match matches.subcommand() {
+    let result = match matches.subcommand() {
         ("get", Some(m)) => handle_get(m),
         ("set", Some(m)) => handle_set(m),
         _ => Ok(()),
     };
 }
 
+fn get_arg(matches: &ArgMatches, arg_name: &str, default_value: &str) -> String {
+    matches.value_of(arg_name)
+           .unwrap_or(default_value)
+           .to_string()
+}
+
+struct GetConfig {
+    credentials_path: String,
+    config_path: String,
+}
+
 fn handle_get(matches: &ArgMatches) -> Result<(), String> {
-    println!("get called {} {}", matches.value_of("credentials-path").unwrap(), matches.value_of("config-path").unwrap());
+    let config = GetConfig {
+        credentials_path: get_arg(matches, "credentials-path", "~/.aws/credentials"),
+        config_path: get_arg(matches, "config-path", "~/.aws/config"),
+    };
+
+    println!("get called {} {}", config.credentials_path, config.config_path);
     Ok(())
 }
 
+struct SetConfig {
+    credentials_path: String,
+    config_path: String,
+    pattern: String,
+}
+
 fn handle_set(matches: &ArgMatches) -> Result<(), String> {
-    println!("set called {} {}", matches.value_of("credentials-path").unwrap(), matches.value_of("config-path").unwrap());
+    let config = SetConfig {
+        credentials_path: get_arg(matches, "credentials-path", "~/.aws/credentials"),
+        config_path: get_arg(matches, "config-path", "~/.aws/config"),
+        pattern: get_arg(matches, "PROFILE_PATTERN", ""),
+    };
+
+    println!("set called {} {} {}", config.credentials_path, config.config_path, config.pattern);
     Ok(())
 }
