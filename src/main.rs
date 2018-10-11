2d9d1c598eae0c01edaed4b709fac38cfe22e652
David Nguyen <hpcsc@users.noreply.github.com>
2018-10-11 17:58:50 +0800

    add clap and initial command line arguments setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+#[macro_use]
+extern crate clap;
+use clap::ArgMatches;
+use clap::{ App };
+
 fn main() {
-    println!("Hello, world!");
+    let yaml = load_yaml!("cli.yaml");
+    let matches = App::from_yaml(yaml).get_matches();
+
+    match matches.subcommand() {
+        ("get", Some(m)) => handle_get(m),
+        ("set", Some(m)) => handle_set(m),
+        _ => Ok(()),
+    };
+}
+
+fn handle_get(matches: &ArgMatches) -> Result<(), String> {
+    println!("get called {} {}", matches.value_of("credentials-path").unwrap(), matches.value_of("config-path").unwrap());
+    Ok(())
+}
+
+fn handle_set(matches: &ArgMatches) -> Result<(), String> {
+    println!("set called {} {}", matches.value_of("credentials-path").unwrap(), matches.value_of("config-path").unwrap());
+    Ok(())
 }
