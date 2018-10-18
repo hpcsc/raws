20195ff54b012e2797323f4d4d66091b1d622790
David Nguyen <hpcsc@users.noreply.github.com>
2018-10-18 17:07:17 +0800

    refactor main logic to use enum and clearer separation of arg parsing

--- src/main.rs
+++ src/main.rs
@@ -7,32 +7,29 @@ fn main() {
     let yaml = load_yaml!("cli.yaml");
     let matches = App::from_yaml(yaml).get_matches();
 
-    let result = match matches.subcommand() {
-        ("get", Some(m)) => handle_get(m),
-        ("set", Some(m)) => handle_set(m),
+    let config = parse_config(&matches);
+
+    let result = match config {
+        Config::Get(config) => get_current_profile(config),
+        Config::Set(config) => set_profile(config),
         _ => Ok(()),
     };
+
+    match result {
+        Err(message) => println!("{}", message),
+        _ => (),
+    }
 }
 
-fn get_arg(matches: &ArgMatches, arg_name: &str, default_value: &str) -> String {
-    matches.value_of(arg_name)
-           .unwrap_or(default_value)
-           .to_string()
+enum Config {
+    Get(GetConfig),
+    Set(SetConfig),
+    Invalid,
 }
 
 struct GetConfig {
     credentials_path: String,
     config_path: String,
-}
-
-fn handle_get(matches: &ArgMatches) -> Result<(), String> {
-    let config = GetConfig {
-        credentials_path: get_arg(matches, "credentials-path", "~/.aws/credentials"),
-        config_path: get_arg(matches, "config-path", "~/.aws/config"),
-    };
-
-    println!("get called {} {}", config.credentials_path, config.config_path);
-    Ok(())
 }
 
 struct SetConfig {
@@ -41,13 +38,41 @@ struct SetConfig {
     pattern: String,
 }
 
-fn handle_set(matches: &ArgMatches) -> Result<(), String> {
-    let config = SetConfig {
-        credentials_path: get_arg(matches, "credentials-path", "~/.aws/credentials"),
-        config_path: get_arg(matches, "config-path", "~/.aws/config"),
-        pattern: get_arg(matches, "PROFILE_PATTERN", ""),
-    };
+fn get_arg(matches: &ArgMatches, arg_name: &str, default_value: &str) -> String {
+    matches.value_of(arg_name)
+           .unwrap_or(default_value)
+           .to_string()
+}
 
+fn get_credentials_path(matches: &ArgMatches) -> String {
+    get_arg(matches, "credentials-path", "~/.aws/credentials")
+}
+
+fn get_config_path(matches: &ArgMatches) -> String {
+    get_arg(matches, "config-path", "~/.aws/config")
+}
+
+fn parse_config(matches: &ArgMatches) -> Config {
+    match matches.subcommand() {
+        ("get", Some(m)) => Config::Get(GetConfig {
+                                credentials_path: get_credentials_path(m),
+                                config_path: get_config_path(m)
+                            }),
+        ("set", Some(m)) => Config::Set(SetConfig {
+                                credentials_path: get_credentials_path(m),
+                                config_path: get_config_path(m),
+                                pattern: get_arg(m, "PROFILE_PATTERN", ""),
+                            }),
+        _ => Config::Invalid
+    }
+}
+
+fn get_current_profile(config: GetConfig) -> Result<(), String> {
+    println!("get called {} {}", config.credentials_path, config.config_path);
+    Ok(())
+}
+
+fn set_profile(config: SetConfig) -> Result<(), String> {
     println!("set called {} {} {}", config.credentials_path, config.config_path, config.pattern);
     Ok(())
 }
