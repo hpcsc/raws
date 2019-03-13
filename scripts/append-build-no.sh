025fc257f96c95474964dcce3c3e66f49b439fd8
David Nguyen <hpcsc@users.noreply.github.com>
2019-03-13 20:11:39 +0800

    change master deployment to bintray

--- scripts/append-build-no.sh
+++ scripts/append-build-no.sh
@@ -8,3 +8,6 @@ if [[ "$BUILD_NO" = "" ]]; then
 fi;
 
 cat Cargo.toml | sed -E 's/^version = "([0-9.-]+)"/version = "\1-'${BUILD_NO}'"/' | tee Cargo.toml
+
+RAWS_VERSION=$(cat Cargo.toml | grep -e '^version = \"[0-9.-]*\"' | sed -E 's/version = "([0-9.-]*)"/\1/')
+cat bintray-descriptor.json | sed -E 's/RAWS_VERSION/'${RAWS_VERSION}'/' | tee bintray-descriptor.json
